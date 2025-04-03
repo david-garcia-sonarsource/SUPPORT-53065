d1b2558333a9a36d6be011b093af914d5d005345
david-garcia-sonarsource <109538884+david-garcia-sonarsource@users.noreply.github.com>
2025-04-03 15:46:44 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,18 @@ public class HelloWorld {
     // Main method - entry point of the program
     public static void main(String[] args) {
         // Print greeting message
-        System.out.println("Hello, World!");
+        printGreeting();
         // Print additional messages
+        printAdditionalInfo();
+    }
+
+    // Method to print greeting message
+    private static void printGreeting() {
+        System.out.println("Hello, World!");
+    }
+
+    // Method to print additional information
+    private static void printAdditionalInfo() {
         System.out.println("Some additional information.");
         System.out.println("Even more information.");
     }
